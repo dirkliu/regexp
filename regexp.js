1091e17ec7dac30e5755c834eae016c19f74cca3
Liu7 <liuqi0702@126.com>
2019-03-21 19:31:55 +0800

    匹配html中的关键字，但是不包含超链接里面的关键字。

--- regexp.js
+++ regexp.js
@@ -3,6 +3,7 @@ export default {
     //密码
     email: /^[A-Za-z0-9\u4e00-\u9fa5_-]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/,
     password: /^(?:(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])|(?=.*[A-Z])(?=.*[a-z])(?=.*[^A-Za-z0-9])|(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])|(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])).{6,12}$/,
-    mobile: /^\d{6,11}$/
+    mobile: /^\d{6,11}$/,
+    htmlKeywordLinked: new RegExp(keyword + '(?!([^<]*>)|([^<]*<\/a>))', 'gm') // 匹配文档中的keyword, 但不包含超链接里面的keyword
   }
 }
